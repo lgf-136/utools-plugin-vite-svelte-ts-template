f80aa734a51e5a93c80b5258e9d7bf49582be952
lgf-136 <86616706+lgf-136@users.noreply.github.com>
2022-09-11 23:06:23 +0800

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+// import sveltePreprocess from 'svelte-preprocess'
+
+// export default {
+//   // Consult https://github.com/sveltejs/svelte-preprocess
+//   // for more information about preprocessors
+//   preprocess: sveltePreprocess(),
+// };
+
+const sveltePreprocess = require('svelte-preprocess');
+
+module.exports = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: sveltePreprocess(),
+};
